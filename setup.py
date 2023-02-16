359f1db9e9997ed2569609e80047fa856c5b9211
meelkkov <meelkkov@gmail.com>
2023-02-16 20:49:24 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+
+import pkg_resources
+from setuptools import setup, find_packages
+
+setup(
+    name="gector",
+    py_modules=["gector"],
+    version="1.0",
+    description="",
+    readme="README.md",
+    python_requires=">=3.7",
+    author="",
+    url="https://github.com/Tequip/gector",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ],
+    include_package_data=True,
+)
