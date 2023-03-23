b9e16fcb8486a5c7498cf6f5cde96c03e48d4473
Monset <monset008@gmail.com>
2023-03-23 12:41:18 +0300

    gector version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name="gector",
     py_modules=["gector"],
-    version="1.5.2",
+    version="1.5.3",
     description="",
     python_requires=">=3.7",
     author="",
